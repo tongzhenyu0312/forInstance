53be38f036e41156d4e087d1ce625605a3012488
tongzhenyu <742502346@qq.com>
2022-02-22 23:11:36 +0800

    hmr-react

---
+++ webpack18-hmr-react/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx';
+
+
+console.log('React: App', App);
+ReactDOM.render(<App/> , document.getElementById('app'));
